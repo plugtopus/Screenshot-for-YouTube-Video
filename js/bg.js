75ddb1c5cd1baecb2c3c9086486a8f50ff339dd6
Your Name <you@example.com>
2018-08-07 10:19:28 +0300

    'init2'

---
+++ js/bg.js
@@ -0,0 +1,8 @@
+chrome.webNavigation.onHistoryStateUpdated.addListener(function (data) {
+    chrome.tabs.get(data.tabId, function (tab) {
+        chrome.tabs.executeScript(data.tabId, {
+            code: 'if (typeof RenderScreenshotButton !== "undefined") { RenderScreenshotButton(); }',
+            runAt: 'document_start'
+        });
+    });
+}, {url: [{hostSuffix: '.youtube.com'}]});
---
+++ js/cs.js
@@ -0,0 +1,78 @@
+function CaptureScreenshot() {
+
+    var appendixTitle = "screenshot.png";
+
+    var title;
+
+    var headerEls = document.querySelectorAll("h1.title");
+
+    function SetTitle() {
+        if (headerEls.length > 0) {
+            title = headerEls[0].innerText.trim();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (SetTitle() == false) {
+        headerEls = document.querySelectorAll("h1.watch-title-container");
+
+        if (SetTitle() == false)
+            title = '';
+    }
+
+    var player = document.getElementsByClassName("video-stream")[0];
+
+    var time = player.currentTime;
+
+    title += " ";
+
+    let minutes = Math.floor(time / 60)
+
+    time = Math.floor(time - (minutes * 60));
+
+    if (minutes > 60) {
+        let hours = Math.floor(minutes / 60)
+        minutes -= hours * 60;
+        title += hours + "-";
+    }
+
+    title += minutes + "-" + time;
+
+    title += " " + appendixTitle;
+
+    var canvas = document.createElement("canvas");
+    canvas.width = player.videoWidth;
+    canvas.height = player.videoHeight;
+    canvas.getContext('2d').drawImage(player, 0, 0, canvas.width, canvas.height);
+
+    var downloadLink = document.createElement("a");
+    downloadLink.download = title;
+
+    canvas.toBlob(function(blob) {
+        downloadLink.href = URL.createObjectURL(blob);
+        downloadLink.click();
+    }, 'image/png');
+}
+
+function RenderScreenshotButton() {
+    var ytpRightControls = document.getElementsByClassName("ytp-right-controls")[0];
+    if (ytpRightControls) {
+        ytpRightControls.prepend(screenshotButton);
+    }
+}
+
+var screenshotButton = document.createElement("button");
+screenshotButton.className = "screenshotButton ytp-button";
+screenshotButton.style.width = "auto";
+screenshotButton.style.font = 'Roboto, medium, sans-serif';
+screenshotButton.style.fontWeight = 'bold';
+screenshotButton.innerHTML = chrome.i18n.getMessage('title');
+screenshotButton.style.backgroundColor = '#ff0000';
+screenshotButton.style.borderRadius = '2px';
+screenshotButton.style.padding = '1px';
+screenshotButton.style.cssFloat = "left";
+screenshotButton.onclick = CaptureScreenshot;
+
+RenderScreenshotButton();
